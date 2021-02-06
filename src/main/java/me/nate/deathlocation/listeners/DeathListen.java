69a2d7b86b588361c4e02921ef9cfa3389509f47
OOF2510 <gamekid01234@gmail.com>
2021-02-06 17:43:11 -0500

    now shows dimension of death

--- src/main/java/me/nate/deathlocation/listeners/DeathListen.java
+++ src/main/java/me/nate/deathlocation/listeners/DeathListen.java
@@ -4,10 +4,13 @@ import me.nate.deathlocation.DeathLocation;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Server;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+
+import static org.bukkit.World.Environment.*;
 
 public class DeathListen implements Listener {
     private DeathLocation plugin;
@@ -20,11 +23,21 @@ public class DeathListen implements Listener {
         Player p = d.getEntity();
         Location deathLoc = p.getLocation();
         Server s = p.getServer();
+        World.Environment di = deathLoc.getWorld().getEnvironment();
         String x = String.valueOf(Math.round(deathLoc.getX()));
         String y = String.valueOf(Math.round(deathLoc.getY()));
         String z = String.valueOf(Math.round(deathLoc.getZ()));
         String ds = x+", "+y+", "+z ;
         String ps = String.valueOf(p.getPlayerListName());
-        s.broadcastMessage(ps+" died at "+ds);
+        if (di == NORMAL) {
+            String dim = "The Overworld";
+            s.broadcastMessage(ps+" died at "+ds+" in "+dim);
+        } else if (di == NETHER) {
+            String dim = "The Nether";
+            s.broadcastMessage(ps+" died at "+ds+" in "+dim);
+        } else if (di == THE_END) {
+            String dim = "The End";
+            s.broadcastMessage(ps+" died at "+ds+" in "+dim);
+        }
     }
 }
